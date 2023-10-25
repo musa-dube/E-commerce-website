850b1ce94f607f5d93d1a93bedd49ab5ccdb96a0
Musa <musamichelledube@gmail.com>
2023-10-25 11:51:52 +0200

    added includes folder and db.inc.php file

---
+++ includes/db.inc.php
@@ -0,0 +1,18 @@
+<?php
+
+    $dsn= "mysql:host=localhost;dbname=careflow_db";
+    $dbusername="root";
+    $dbpassword="";
+  
+    try
+    {
+        //php data objects = pdo, flexabile connections to may types of db
+        $pdo = new PDO($dsn, $dbusername, $dbpassword); // NB line to connect to db
+        $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // throw an excpetion
+    }
+    catch(PDOException $exp)
+    {
+        echo "Connection failed: " . $exp->getMessage(); //display error in browser 
+    }
+
+    
